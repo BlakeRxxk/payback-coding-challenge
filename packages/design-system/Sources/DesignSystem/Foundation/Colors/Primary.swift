553a1b6542020c7e17809c0d18575e85a8672ab1
Blake <3397261+BlakeRxxk@users.noreply.github.com>
2026-09-11 22:47:48 +0200

    feat(design-system): update primary colors from Figma

--- packages/design-system/Sources/DesignSystem/Foundation/Colors/Primary.swift
+++ packages/design-system/Sources/DesignSystem/Foundation/Colors/Primary.swift
@@ -3,7 +3,7 @@ import SwiftUI
 extension Color {
     /// Colors used for logos and icons
     public enum Primary {
-        public static let primary1 = Color(red: 0.96, green: 0.28, blue: 0.37)
-        public static let primary2 = Color(red: 1, green: 0.22, blue: 0.36)
+        public static let primary1 = Color(red: 0.965, green: 0.278, blue: 0.373)
+        public static let primary2 = Color(red: 1, green: 0.22, blue: 0.361)
     }
 }
