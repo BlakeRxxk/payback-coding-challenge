d0e89017f365e6efbd336ed1922300e4fff07896
Blake <3397261+BlakeRxxk@users.noreply.github.com>
2026-09-11 22:48:06 +0200

    feat(design-system): update accent colors from Figma

--- packages/design-system/Sources/DesignSystem/Foundation/Colors/Accents.swift
+++ packages/design-system/Sources/DesignSystem/Foundation/Colors/Accents.swift
@@ -3,9 +3,9 @@ import SwiftUI
 extension Color {
     /// Colors used for icons, links.
     public enum Accents {
-        public static let accent1 = Color(red: 0.96, green: 0.84, blue: 0.87)
-        public static let accent2 = Color(red: 0.82, green: 0.21, blue: 0.38)
-        public static let discount = Color(red: 0, green: 0.54, blue: 0.02)
-        public static let link = Color(red: 0, green: 0.3, blue: 0.77)
+        public static let accent1 = Color(red: 0.965, green: 0.843, blue: 0.875)
+        public static let accent2 = Color(red: 0.816, green: 0.212, blue: 0.376)
+        public static let discount = Color(red: 0, green: 0.541, blue: 0.02)
+        public static let link = Color(red: 0, green: 0.298, blue: 0.769)
     }
 }
