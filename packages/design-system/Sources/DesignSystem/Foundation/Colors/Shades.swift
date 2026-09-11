a71715f34cb4554ad77ce4914b921a9787870f9a
Blake <3397261+BlakeRxxk@users.noreply.github.com>
2026-09-11 22:48:18 +0200

    feat(design-system): update shade colors from Figma

--- packages/design-system/Sources/DesignSystem/Foundation/Colors/Shades.swift
+++ packages/design-system/Sources/DesignSystem/Foundation/Colors/Shades.swift
@@ -4,10 +4,10 @@ extension Color {
     /// Colors used for backgrounds, text, dividers, etc.
     public enum Shades {
         public static let shade1 = Color.white
-        public static let shade2 = Color(red: 0.13, green: 0.13, blue: 0.13)
+        public static let shade2 = Color(red: 0.133, green: 0.133, blue: 0.133)
         /// shade2 with opacity 5%
-        public static let shade3 = Color(red: 0.13, green: 0.13, blue: 0.13).opacity(0.05)
+        public static let shade3 = Color(red: 0.133, green: 0.133, blue: 0.133).opacity(0.05)
         /// shade2 with opacity 30%
-        public static let shade4 = Color(red: 0.13, green: 0.13, blue: 0.13).opacity(0.3)
+        public static let shade4 = Color(red: 0.133, green: 0.133, blue: 0.133).opacity(0.3)
     }
 }
